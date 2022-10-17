4f4d3b3213ebded84fbe36ba82863f26dc2c0539
rohit <rohit@extramile.in>
2022-10-17 14:22:07 +0530

    changes

--- dao/config.php
+++ dao/config.php
@@ -1,21 +1,21 @@
 <?php
 
 
-
+$database = "extramileplay_dhoom_dham_diwali";
 if (file_exists("../../env.php")) {
     include_once("../../env.php");
 }
 else {
     include_once("../env.php");
 }
-$database = "extramileplay_dhoom_dham_diwali";
+
 
 
 $con=mysqli_connect($server,$username,$password,$database) or die("please check your database connection");
-try {
-    $connPdo = new PDO("mysql:host=" . $server . ";dbname=" . $database . "", "" . $username . "", "" . $password . "");
-    $connPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    Logger::info("Error: Could not make a connection" . $e->getMessage());
-    exit("Error: Could not make a connection" . $e->getMessage());
-}
+// try {
+//     $connPdo = new PDO("mysql:host=" . $server . ";dbname=" . $database . "", "" . $username . "", "" . $password . "");
+//     $connPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     Logger::info("Error: Could not make a connection" . $e->getMessage());
+//     exit("Error: Could not make a connection" . $e->getMessage());
+// }
